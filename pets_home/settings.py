b976609ba4180d6d6b013d1594396c5500946e2f
Nikita <Bondarchuknana@gmail.com>
2021-08-26 16:16:53 +0300

    init

--- pets_home/settings.py
+++ pets_home/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'pets_home.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
