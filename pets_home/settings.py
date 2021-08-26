9eb4b36b87f888a73a64e88bd1a7e26f9088bdbe
Nikita <Bondarchuknana@gmail.com>
2021-08-26 16:14:18 +0300

    init

--- pets_home/settings.py
+++ pets_home/settings.py
@@ -90,7 +90,8 @@ WSGI_APPLICATION = 'pets_home.wsgi.application'
 # }
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
